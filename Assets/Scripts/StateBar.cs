bf44dfc6d30a028137ceff59cc1e1e11c4ae9d0f
mek4nr <jbaptiste.munieres@gmail.com>
2017-09-22 22:52:12 -0500

    State bare script with prefab for UI

---
+++ Assets/Scripts/StateBar.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StateBar : MonoBehaviour {
+
+    public Text TextName;
+    public Scrollbar ScrollLeft;
+    public Scrollbar ScrollRight;
+
+    public Image HandleLeft;
+    public Image HandleRight;
+
+    public Color Left = Color.red;
+    public Color Right = Color.green;
+
+    public float StateValue;
+    public string StateName;
+
+    public void Init()
+    {
+        TextName.text = StateName;
+        HandleLeft.color = Left;
+        HandleRight.color = Right;
+    }
+
+    private void Awake()
+    {
+        Init();
+    }
+
+    public void SetState(float stateValue)
+    {
+        ScrollLeft.size = -stateValue;
+        ScrollRight.size = stateValue;
+    }
+
+
+    private void OnValidate()
+    {
+        SetState(StateValue);
+        Init();
+    }
+
+}
