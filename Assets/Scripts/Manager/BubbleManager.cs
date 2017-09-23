1f2f63e7d017b7621fb74cdf542abb0591808583
Magicolo <straty91@hotmail.com>
2017-09-23 15:07:29 -0400

    dialog

--- Assets/Scripts/Manager/BubbleManager.cs
+++ Assets/Scripts/Manager/BubbleManager.cs
@@ -4,10 +4,16 @@ public class BubbleManager : Singleton<BubbleManager>
 {
 	public GameObject prefabs;
 
+<<<<<<< b707711ccadad04ff77566c2a2a845155fbe47b5
 	public void Spawn(float width, float height, string textToWrite, float speed, float timeBeforeHidding, GameObject attachTo, bool left)
     {
+=======
+	public GameObject Spawn(string textToWrite, float speed, float timeBeforeHidding, GameObject attachTo, float width, float height, bool leftBubble)
+	{
+>>>>>>> 03128ceb75682806e0ff7a0e9a501e0caead39cf
 		var newBubble = Instantiate(prefabs);
 
+<<<<<<< b707711ccadad04ff77566c2a2a845155fbe47b5
         newBubble.transform.SetParent(attachTo.transform);
         newBubble.GetComponent<RectTransform>().sizeDelta = new Vector2(width, height);
         newBubble.GetComponent<RectTransform>().localScale = Vector3.one;
@@ -29,4 +35,11 @@ public class BubbleManager : Singleton<BubbleManager>
 
         print(newBubble.transform.position);
     }
+=======
+		newBubble.GetComponent<UIBubbleText>().Init(textToWrite, speed, timeBeforeHidding, leftBubble);
+		newBubble.GetComponent<UIBubbleText>().LaunchBubble();
+
+		return newBubble;
+	}
+>>>>>>> 03128ceb75682806e0ff7a0e9a501e0caead39cf
 }
