fcdf6cd845d4cd3b529bb57ee323e2980b56cece
mek4nr <jbaptiste.munieres@gmail.com>
2017-09-23 00:19:43 -0500

    push time data for hour & minutes + mushroom UI + debut UI time line

---
+++ Assets/Scripts/TimeData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class TimeData
+{
+    int hours;
+    int minutes;
+
+    public static TimeData operator -(TimeData a, TimeData b)
+    {
+        TimeData t = new TimeData();
+
+        t.hours = a.hours - b.hours;
+        t.minutes = a.minutes - b.minutes;
+
+        if(t.minutes < 0)
+        {
+            t.minutes = 60 - t.minutes;
+            t.hours--;
+        }
+
+        return t;
+    }
+
+    public int ToMinutes()
+    {
+        return hours * 60 + minutes;
+    }
+}
---
+++ Assets/Scripts/UITimeLine.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Scrollbar))]
+public class UITimeLine : MonoBehaviour
+{
+    public TimeData timeBegin;
+    public int CompleteTime;
+
+    private Scrollbar currentScroll;
+
+    private void Start()
+    {
+        currentScroll = GetComponent<Scrollbar>();
+    }
+
+    public TimeData CurrentTime()
+    {
+        return null;
+    }
+
+    public float CalculValue()
+    {
+        return (CurrentTime() - timeBegin).ToMinutes() / CompleteTime;
+    }
+
+    private void Update()
+    {
+        currentScroll.size = CalculValue();
+    }
+}
