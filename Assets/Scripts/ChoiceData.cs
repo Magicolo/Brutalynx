00f1fcf3a2b57f67deaed57e9e18051ee9775b8d
mek4nr <jbaptiste.munieres@gmail.com>
2017-09-23 11:56:37 -0500

    need pull

---
+++ Assets/Scripts/ChoiceData.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System;
+using UnityEngine.Events;
+using System.Collections.Generic;
+
+[Serializable]
+public class ChoiceData
+{
+    public string text;
+    public List<UnityEvent> onClick = new List<UnityEvent>();
+}
---
+++ Assets/Scripts/UI/UIContextMenu.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using Utils.UI;
+using System.Collections.Generic;
+
+public class UIContextMenu : UIPanel
+{
+    public List<ChoiceData> choices = new List<ChoiceData>();
+    public UITimeLine timeLine;
+
+    public void Init(List<ChoiceData> choices)
+    {
+        Hide();
+        this.choices = choices;
+    }
+
+    public void LaunchContext()
+    {
+
+    }
+}
