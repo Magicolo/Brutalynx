eb2b47917961689486e03e58de2da6ce97fdb0cc
Magicolo <straty91@hotmail.com>
2017-09-23 00:14:19 -0400

    du temps pis toute

---
+++ Assets/Scripts/Events/TelephoneEvent.cs
@@ -0,0 +1,14 @@
+﻿public class TelephoneEvent : EventBase
+{
+	public EventBase Next;
+
+	public override bool IsAvailable(Mushrooms mushroom)
+	{
+		return true;
+	}
+
+	public override EventBase Select(Mushrooms mushroom)
+	{
+		return Next;
+	}
+}
--- Assets/Scripts/StoryManager.cs
+++ Assets/Scripts/StoryManager.cs
@@ -5,6 +5,7 @@ public class StoryManager : MonoBehaviour
 {
 	public static StoryManager Instance { get; private set; }
 
+	public float Time { get; private set; }
 	public EventBase Current;
 
 	public readonly Stack<Mushrooms> History = new Stack<Mushrooms>();
@@ -14,8 +15,29 @@ public class StoryManager : MonoBehaviour
 		Instance = this;
 	}
 
+	void Update()
+	{
+		Time += UnityEngine.Time.deltaTime;
+	}
+
 	public void Select(Mushrooms mushroom)
 	{
+		// Add the logic for the effects of mushrooms.
+		// Must be here because it might depend on past consumption.
+		//switch (mushroom)
+		//{
+		//	case Mushrooms.Red:
+		//		if (History.Peek() == Mushrooms.Green)
+		//			PlayerManager.Instance.Boredom += 10259847;
+		//		break;
+		//	case Mushrooms.Green:
+		//		break;
+		//	case Mushrooms.Yellow:
+		//		break;
+		//	case Mushrooms.Blue:
+		//		break;
+		//}
+
 		History.Push(mushroom);
 		Current.gameObject.SetActive(false);
 		Current = Current.Select(mushroom);
