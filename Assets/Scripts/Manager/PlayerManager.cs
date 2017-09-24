746f7df0ff0aab7cd4f6de471ac1db935d787e85
Magicolo <straty91@hotmail.com>
2017-09-23 21:51:02 -0400

    asoifug

--- Assets/Scripts/Manager/PlayerManager.cs
+++ Assets/Scripts/Manager/PlayerManager.cs
@@ -5,19 +5,19 @@ using UnityEngine;
 public enum Statuses
 {
 	Default,
-	Depressive,
-	Anxious,
-	Drowsy,
-	Alert,
-	Paranoid,
-	Narcisic
+	Tristesse,
+	Extase,
+	Terreur,
+	Narcicique,
+	Rage,
+	Fanatisme,
 }
 
 public enum Traits
 {
-	Confidence,
 	Happiness,
-	Alertness
+	Confidence,
+	Irritability
 }
 
 public struct StatusEffect
@@ -31,60 +31,32 @@ public struct StatusEffect
 public class PlayerManager : Singleton<PlayerManager>
 {
 	[Range(-1f, 1f)]
-	public float Confidence;
-	[Range(-1f, 1f)]
 	public float Happiness;
 	[Range(-1f, 1f)]
-	public float Alertness;
+	public float Confidence;
+	[Range(-1f, 1f)]
+	public float Irritability;
 
 	public bool IsParanoiac { get { return Confidence <= -1f; } }
 	public bool IsDepressed { get { return Happiness <= -1f; } }
-	public bool IsDrowsy { get { return Alertness <= -1f; } }
+	public bool IsDrowsy { get { return Confidence <= -1f; } }
 	public bool IsNarcisic { get { return Confidence >= 1f; } }
 	public bool IsEuphoric { get { return Happiness >= 1f; } }
-	public bool IsOversensitive { get { return Alertness >= 1f; } }
-
+	public bool IsOversensitive { get { return Confidence >= 1f; } }
 
 	public bool IsStatus(Statuses status)
 	{
-		return true;
+		switch (status)
+		{
+			case Statuses.Terreur: return Confidence <= -0.5f;
+			case Statuses.Rage: return Irritability <= -0.5f;
+			case Statuses.Tristesse: return Happiness <= -0.5f;
+			case Statuses.Extase: return Happiness >= 0.5f;
+			case Statuses.Narcicique: return Confidence <= 0.5f;
+			case Statuses.Fanatisme: return Irritability >= 0.5f;
+			case Statuses.Default: return true;
+		}
+
+		return false;
 	}
-	//readonly List<StatusEffect> _effects = new List<StatusEffect>();
-
-	//void Update()
-	//{
-	//	UpdateEffects();
-	//}
-
-	//void UpdateEffects()
-	//{
-	//	for (int i = 0; i < _effects.Count; i++)
-	//	{
-	//		var effect = _effects[i];
-	//		var deltaTime = TimelineManager.Instance.DeltaTime;
-	//		effect.Delay = Utility.Max(effect.Delay - TimelineManager.Instance.DeltaTime, TimeSpan.Zero);
-	//		var modifier = effect.Delay <= TimeSpan.Zero ?
-	//			Math.Min(TimelineManager.Instance.DeltaTime.TotalSeconds / effect.Time.TotalSeconds, Math.Abs(effect.Remaining)) : 0f;
-	//		modifier *= Math.Sign(effect.Remaining);
-	//		effect.Remaining -= modifier;
-
-	//		switch (effect.Status)
-	//		{
-	//			case Status.Confidence:
-	//				Confidence += modifier;
-	//				break;
-	//			case Status.Happiness:
-	//				Happiness += modifier;
-	//				break;
-	//			case Status.Alertness:
-	//				Alertness += modifier;
-	//				break;
-	//		}
-
-	//		if (Math.Abs(effect.Remaining) <= 0.0) _effects.RemoveAt(i--);
-	//		else _effects[i] = effect;
-	//	}
-	//}
-
-	//public void AddEffect(StatusEffect effect) { _effects.Add(effect); }
 }
