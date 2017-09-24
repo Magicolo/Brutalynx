202cfd8b1eeb902ffd534989388d12756c71a74c
mek4nr <jbaptiste.munieres@gmail.com>
2017-09-23 21:30:40 -0500

    Add flicker on tv, with prefab

---
+++ Assets/Scripts/UI/UIFlicker.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class UIFlicker : MonoBehaviour {
+
+    public Image img;
+    private Color color;
+
+    private void Start()
+    {
+        img = GetComponent<Image>();
+        color = img.color;
+    }
+    // Update is called once per frame
+    void Update () {
+
+        color.a += Random.value - 0.5f;
+        color.a = Mathf.Clamp(color.a, 0, 1);
+        img.color = color;
+	}
+}
