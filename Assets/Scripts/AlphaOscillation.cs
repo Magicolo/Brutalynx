c3796bf09d9583dfe8b8397f35a72f4783db0bc8
Magicolo <straty91@hotmail.com>
2017-09-24 09:00:14 -0400

    wife

---
+++ Assets/Scripts/AlphaOscillation.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class AlphaOscillation : MonoBehaviour
+{
+	public float Frequency = 2f;
+	public float Amplitude = 0.25f;
+	public float Center = 1f;
+	public Image Image;
+
+	void Update()
+	{
+		var color = Image.color;
+		var sine = Mathf.Sin(Time.time * Frequency + GetHashCode()) * Amplitude + Center;
+		color.a = sine;
+		Image.color = color;
+	}
+}
+
---
+++ Assets/Scripts/ScaleOscillation.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class ScaleOscillation : MonoBehaviour
+{
+	public float Frequency = 2f;
+	public float Amplitude = 0.25f;
+	public float Center = 1f;
+
+	void Update()
+	{
+		var sine = Mathf.Sin(Time.time * Frequency + GetHashCode()) * Amplitude + Center;
+		var scale = Vector3.one * sine;
+		transform.localScale = scale;
+	}
+}
+
